6711ac9a62a2af1b8f47e84b98908184a49b9f3e
jdiaz <jayremie.diaz@gmail.com>
2025-01-25 20:57:01 +0800

    Update page.tsx

--- projects/page.tsx
+++ projects/page.tsx
@@ -8,7 +8,7 @@ import { ArrowRight, Terminal } from 'lucide-react';
 const projects = [
   {
     title: "SalesX AI", 
-    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/jeremie_diaz/app/images/s-o-c-i-a-l-c-u-t-7KkDiSs5UdQ-unsplash.jpg?w=800&h=600&fit=crop",
+    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/main/app/images/s-o-c-i-a-l-c-u-t-7KkDiSs5UdQ-unsplash.jpg?w=800&h=600&fit=crop",
     problem: "Businesses often struggle to make accurate sales forecasts, leading to inefficient planning and missed growth opportunities. Analyzing historical sales data and predicting future trends is time-consuming and requires specialized expertise.",
     solution: "SalesX simplifies sales forecasting by using advanced analytics to process sales data and generate precise forecasts for the next 12 months. With user-friendly insights and actionable predictions, businesses can make data-driven decisions, optimize strategies, and stay ahead of the competition.",    
     tools: ["Python", "OpenAI", "LLM", "NLG", "Streamlit"],
@@ -17,7 +17,7 @@ const projects = [
   },
   {
     title: "Web Analytics AI",
-    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/jeremie_diaz/app/images/andy-kelly-0E_vhMVqL9g-unsplash.jpg?w=800&h=600&fit=crop",
+    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/main/app/images/andy-kelly-0E_vhMVqL9g-unsplash.jpg?w=800&h=600&fit=crop",
     problem: "Businesses often face challenges in extracting meaningful insights from complex web analytics datasets. Traditional tools require significant manual effort and expertise, leading to missed opportunities and inefficiencies in decision-making.",
     solution: "Web Analytics X leverages AI-powered tools built on OpenAI's Swarm and Firecrawl frameworks to transform web analytics. The AI agent performs advanced data analysis, generates visualizations, and delivers actionable insights and reports. This empowers businesses to make smarter, data-driven decisions with ease and efficiency.",
     tools:["Python", "OpenAI", "LLM", "Swarm", "Streamlit"],  
@@ -25,7 +25,7 @@ const projects = [
   },
   {
     title: "RouteX",
-    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/jeremie_diaz/app/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg?w=800&h=600&fit=crop",
+    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/main/app/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg?w=800&h=600&fit=crop",
     problem: "Managing delivery routes can be complex and inefficient, leading to increased costs, delays, and dissatisfied customers. Businesses often lack the tools to optimize routes effectively for time, cost, and resource utilization.",
     solution: "RouteX streamlines delivery logistics by analyzing and optimizing delivery routes in real-time. Using advanced algorithms, the app reduces travel time, cuts costs, and ensures timely deliveries, enabling businesses to enhance operational efficiency and customer satisfaction.",
     tools:["Python", "OpenAI",  "LLM", "Streamlit"]
@@ -33,7 +33,7 @@ const projects = [
   },
   {
     title: "News Summarizer AI",
-    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/jeremie_diaz/app/images/mr-cup-fabien-barral-Mwuod2cm8g4-unsplash.jpg?w=800&h=600&fit=crop",
+    image: "https://github.com/jaydiaz2012/Jeremie_Diaz_AI_DS_Portfolio/blob/main/app/images/mr-cup-fabien-barral-Mwuod2cm8g4-unsplash.jpg?w=800&h=600&fit=crop",
     problem: "Staying informed is challenging when news articles are lengthy and time-consuming to read. Busy individuals often struggle to keep up with the latest developments without dedicating significant time.",
     solution: "News Summarizer AI simplifies news consumption by summarizing any article from a website link into concise, easy-to-read summaries. With NewsAI, users can stay updated quickly and efficiently, saving time while accessing the essential information they need.",
     tools: ["Python", "OpenAI", "LLM"],
